7e97ca7a3bcc80d3975dab789d51c170e6e2595e
tenor2000 <tenor2000@gmail.com>
2024-01-08 12:24:06 -0500

    update image request

--- main.js
+++ main.js
@@ -2,25 +2,29 @@ var contents = $('#content');
 const contentsParent = contents.parent()
 contents.remove();
 
-var text = $('<p>').text('Get back to achieving your coding dreams!');
-var image = $('<img>', {
-    src: 'https://img.freepik.com/free-vector/page-found-concept-illustration_114360-1869.jpg?size=626&ext=jpg',
-    alt: 'Image Alt Text'
-})
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+var text = $('<p>').text('Get back to achieving your coding dreams!');  
 
 $.ajax({
     method: 'GET',
     url: 'https://picsum.photos/list',
     success: function(result) {
      // result is whatever the URL sends back from the request
-        console.log(result);
+        var randnum = Math.floor(getRandomArbitrary(0, result.length));
+        var image = $('<img>', {
+            src: `https://picsum.photos/id/${randnum}/1200/800`,
+            alt: 'Image Alt Text'
+        })
+        text.addClass('beautText');
+        contentsParent.prepend(image);
+        contentsParent.prepend(text);
     },
     error: function(err) {
      // if any errors occur during the process you can check out the
      // the error by logging the 'err' argument
+        console.log(err)
     }
-  });
-text.addClass('beautText');
-contentsParent.prepend(image);
-contentsParent.prepend(text);
-
+});
